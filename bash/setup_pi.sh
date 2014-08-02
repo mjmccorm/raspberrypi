7557d0f3d45f2c6e3b2e225855842eb5ca0d34ba
Matt McCormick <mjmccorm@gmail.com>
2014-08-02 03:33:20 +0000

    more changes to setup_pi

--- bash/setup_pi.sh
+++ bash/setup_pi.sh
@@ -1,7 +1,14 @@
 #Update the PI
 sudo rpi-update
 
+#Setup bash and VI so backspace/delete work properly
+
+#Set keyboard to English (US)
+echo "You need to set keyboard to English(US)"
+sudo raspi-config
+
 #Install SimpleCV
+sudo apt-get install ipython python-opencv python-scipy python-numpy python-setuptools python-pip
 
 #Install PiCamera
 sudo apt-get install python-picamera
