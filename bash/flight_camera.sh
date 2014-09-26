e92dc6cd2cf48e7ce3a5c377da43f57883f6a078
Matt McCormick <mjmccorm@gmail.com>
2014-09-26 02:35:21 +0000

    Added parameters to flight_camera

--- bash/flight_camera.sh
+++ bash/flight_camera.sh
@@ -6,8 +6,8 @@
 #Change directories to output dir
 cd ~/test
 
-#take photos every 2.5s for 25s duration
+#take photos every 3s for 60s duration
 #2s seems to skip frames.
-raspistill -o myimage_%d.jpg -tl 2500 -t 25000
+raspistill --exposure fixedfps --nopreview --raw -o myimage_%d.jpg -tl 4000 -t 60000
 
 #potentially reduce exposure time  to decrese time between photos.
